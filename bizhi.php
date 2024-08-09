0c57f4e0f7bd0e9a6eac77bafa946aacfd2dfdd8
smile <154714821+badboy518714@users.noreply.github.com>
2024-08-09 23:20:50 +0800

    Update bizhi.php

--- bizhi.php
+++ bizhi.php
@@ -1,4 +1,27 @@
-<?php
-header('Content-Type: image/jpeg');
-readfile('https://badboy518714.github.io/happy.github.io/img3.webp');
-?>
+<h1>图片类型不能为空</h1>
+<hr/>
+<h4>输入参数以继续：</h4>
+<p>
+    <b>type</b>
+    ：空/json/xml/url/302(非必须填写）
+</p>
+<p>
+    <b>px</b>
+    ：pc/m  留空自适应屏幕分辨率(非必须填写）
+<p>
+    <b>category</b>
+    ：要获取的图片类型《dongman》《fengjing》《biying》《meinv》(必须填写）
+</p>
+<hr/>
+<p>
+    <b>动漫电脑图片302跳转示例</b>
+    ：<a href="?category=dongman&type=302" target="_blank">https://tuapi.eees.cc/api.php?category=dongman&type=302</a>
+</p>
+<p>
+    <b>动漫手机图片302跳转示例</b>
+    ：<a href="?category=dongman&px=m&type=302" target="_blank">https://tuapi.eees.cc/api.php?category=dongman&px=m&type=302</a>
+</p>
+<p>
+    <b>图片混合示例：包括动漫，风景图片</b>
+    ：<a href="https://tuapi.eees.cc/api.php?category={dongman,fengjing}" target="_blank">https://tuapi.eees.cc/api.php?category={dongman,fengjing}</a>
+</p>
