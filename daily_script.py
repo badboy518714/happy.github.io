3847a373fcdf0808ce0f85c2c5bc568d8730d297
smile <154714821+badboy518714@users.noreply.github.com>
2024-09-06 05:30:02 +0800

    Update daily_script.py

--- daily_script.py
+++ daily_script.py
@@ -6,5 +6,5 @@ print('hello world')
 print('hello world')
 
 #open('https://raw.githubusercontent.com/badboy518714/happy.github.io/main/output.txt', 'w+').write(f'helloworld{int(time.time()*1000)}')
-with open('https://raw.githubusercontent.com/badboy518714/happy.github.io/main/output.txt', 'w+') as f:
+with open('output.txt', 'w+') as f:
    f.write('helloworld')
