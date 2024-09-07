84bc92ab1c76d4b57f718fb492670e1971979ef4
badboy518714 <154714821+badboy518714@users.noreply.github.com>
2024-09-08 04:19:00 +0800

    Update update.py

--- update.py
+++ update.py
@@ -5,8 +5,8 @@ def update_txt_file():
     file_path = "0.txt"  # 替换为实际的文件路径
 
     # 读取文件内容
-    with open(file_path, "r") as file:
-        content = file.read()
+    # with open(file_path, "r") as file:
+    #     content = file.read()
 
     # 更新内容（这里只是简单地添加一些文本）
     updated_content =  f"{int(time.time()*1000)}--This is an updated line."
